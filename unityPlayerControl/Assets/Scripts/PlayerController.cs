258d49d3dd5b82b59a792ee9ff0378b6fa233499
wyliekasai <wylie.kasai@dali.dartmouth.edu>
2021-09-29 14:20:40 -0400

    base project (lost commits on old repo)

---
+++ unityPlayerControl/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float forwardInput;
+    private float horizontalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get inputs from WASD or arrow keys - see InputManager
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // car's forward movement, uses forwardInput
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // car's turning mechanism, uses horizontalInput
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
